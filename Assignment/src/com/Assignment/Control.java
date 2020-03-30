11193bf2b697c4a15f3426f403b580f4a150493c
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-03-30 11:55:39 +0100

    30.03.2020

---
+++ Assignment/src/com/Assignment/Control.java
@@ -0,0 +1,9 @@
+package com.Assignment;
+
+public class Control {
+	
+	public static void main(String args[]) {
+		Screen main = new Screen("Topic Modeller");
+	}
+	
+}
---
+++ Assignment/src/com/Assignment/Screen.java
@@ -0,0 +1,17 @@
+package com.Assignment;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+public class Screen extends JFrame{
+	
+	public Screen(String Title) {
+		super(Title);
+		
+		setSize(450,450);
+		setLayout(new BorderLayout());
+		
+		setVisible(true);
+	}
+	
+	
+
+}
