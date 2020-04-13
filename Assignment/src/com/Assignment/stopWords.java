34a6fac30c8642b5753d906ef2b096bb3446f1d9
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-04-13 14:56:51 +0100

    commit

--- Assignment/src/com/Assignment/stopWords.java
+++ Assignment/src/com/Assignment/stopWords.java
@@ -15,43 +15,64 @@ public class stopWords extends File{
 		//Retrieving the contents of the specified page
 		Scanner website = new Scanner(url.openStream());
 		//Instantiating the StringBuffer class to hold the result
-		StringBuffer sb = new StringBuffer();
+		StringBuffer buffer = new StringBuffer();
 		while(website.hasNext()) {
-			sb.append(website.next());
+			buffer.append(website.next());
 		}
 		website.close();
 		//Retrieving the String from the String Buffer object
-		String result = sb.toString();
+		String outcome = buffer.toString();
 		
 		//Removing the HTML tags and unnecessary symbols
-		result        = result.replaceAll("<[^>]*>", " ");
-		result        = result.replace("&nbsp", " ");
-		result        = result.replace("#", " ");
+		outcome      = outcome.replaceAll("<[^>]*>", " ");
+		outcome      = outcome.replace("&nbsp", " ");
+		outcome     = outcome.replace("#", " ");
 	    
-		String words = (String) result.subSequence(946,3002);
+		String result = (String) outcome.subSequence(946,3002);
+		
+		char[] words = result.toCharArray();
 		
 		//find the locations of the words where they begin
-		int[] startOfWord = new int[] {1,6,};
+		int[] startOfWord = new int[] {0,1,6,11,17,22,27,34,37,43,48,53,60,64,72,78,83,85,88,95,98,105,111,119,127,130,134,139,145,147,150,155,161,165,167,171};
 		
 		//create a loop that creates the letter to go uppercase if it begins at the word
+				
+		for(i = 0; i < words.length; i++){
+	    	for(j=0;j<startOfWord.length;j++) {
+	    		if(i==startOfWord[j]) {
+	    			words[i] = UpperCase(words[i]);
+	    		}
+	    	}
+	    }
 		
+		System.out.println(words);
 		//then split the string from the word are the words should be put into a string
 		
 		//look at main.java in lab7 if confused and use that code
 		
 		
-		//System.out.println(words);
-		//newWords = words;
 		
-		for(i=0;i<newWords.length;i++) {
-			System.out.println(newWords[i]);
-		}
-		for(i=0;i<newWords.length;i++) {
+		
+		//for(i=0;i<newWords.length;i++) {
+		//	System.out.println(newWords[i]);
+		//}
+		//for(i=0;i<newWords.length;i++) {
 			
-		}
+		//}
 		
 	}
+
 	
+	public static char UpperCase(char letter) {
+
+		    char word = Character.toUpperCase(letter);
+		    
+		    return word;
+
+	}
+		
+	
+
 	public stopWords(File f1, File f2) {
 		this.f1=f1;
 		this.f1=f2;
