ecb93dd76c72a2b400f0947c3aa7e8772d51cd49
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-04-18 16:31:34 +0100

    Buttons started. Layout needs to be changed

--- Assignment/src/com/Assignment/Files.java
+++ Assignment/src/com/Assignment/Files.java
@@ -3,40 +3,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Files {
-	private String title1;
-	private String title2;
-	File f1,f2;
-	Scanner s1,s2;
-	static String token1,token2;
+	private String title;
+	File f1;
+	Scanner s1;
+	
 
-	public Files(String title1,String title2) {
-		this.title1=title1;
-		this.title2=title2;
+	public Files(String title) {
+		this.title=title;
 	}
 
 
-	public String getTitle1() {
-		return title1;
+	public String getTitle() {
+		return title;
 	}
 
 
-	public void setTitle1(String title1) {
-		this.title1 = title1;
-	}
-
-
-	public String getTitle2() {
-		return title2;
-	}
-
-
-	public void setTitle2(String title2) {
-		this.title2 = title2;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	
 	void open() {
-		f1 = new File(this.title1);
-		f2 = new File(this.title2);
+		f1 = new File(this.title);
 	}
 	
 	public static String read(Scanner file) {
@@ -49,8 +36,6 @@ public class Files {
 	}
 	void close() {
 		s1.close();
-		s2.close();
-		
 	}
 	
 	
--- Assignment/src/com/Assignment/Screen.java
+++ Assignment/src/com/Assignment/Screen.java
@@ -1,13 +1,20 @@
 package com.Assignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.Button;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 public class Screen extends JFrame implements ActionListener{
 	
 	static String title = null;
-	Button b1,b2;
+	JButton b1,b2,b3;
+	JPanel south,east,west,north;
+	Files f1,f2;
+	JTextField t1,t2;
 	public static void main(String[] args) {
 		Screen main = new Screen("Topic Modeller");
 	}
@@ -15,17 +22,45 @@ public class Screen extends JFrame implements ActionListener{
 	public Screen(String title) {
 		super(title);
 		
-		setSize(750,750);
+		setSize(750,550);
 		setLayout(new BorderLayout());
+		setResizable(false);
+		
+		b1 =  new JButton("Check if 1st file exists");
+		b2 = new JButton("Check if 2nd file exists");
+		
+		t1 = new JTextField("Enter 1st File here");
+		t2 = new JTextField("Enter 2nd File here");
+		
+		north = new JPanel();
+		south = new JPanel();
+		east = new JPanel();
+		west = new JPanel();
 		
 		
+		west.add(b1);
+		east.add(b2);
+		south.add(b3);
+		
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		
+		add(north, BorderLayout.NORTH);
+		add(south, BorderLayout.SOUTH);
+		add(east, BorderLayout.EAST);
+		add(west, BorderLayout.WEST);
 		
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource()== b1) {
+			f1 = new Files(t1.getText());
+		}
+		else if (e.getSource()==b2) {
+			f1 = new Files(t2.getText());
+		}
 		
 	}
 	
